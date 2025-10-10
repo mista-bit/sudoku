027314800a061c0189d62b0ea80201f0936d74dc
mista-bit <pedroucn2007@gmail.com>
2025-10-10 08:16:15 -0300

    ...

---
+++ sudoku/sudoku.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define LINHAS 9
+#define COLUNAS 9
+
+int gerarSudoku(){
+    
+    return 0;
+}
+
+int main(void) {
+    int sudoku[LINHAS][COLUNAS] = {
+    {0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0}
+    };
+    /* use gerarSudoku() as needed */
+    return 0;
+}
