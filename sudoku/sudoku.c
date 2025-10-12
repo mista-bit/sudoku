d009884de1ae05fe51f16f9b8d65affa0163c7a1
mista-bit <pedroucn2007@gmail.com>
2025-10-12 17:14:38 -0300

    :sparkles: feat: Criei as funções para validar se uma posição é válida e para procurar o próximo espaço vazio

--- sudoku/sudoku.c
+++ sudoku/sudoku.c
@@ -1,24 +1,49 @@
 #include <stdio.h>
-#define LINHAS 9
-#define COLUNAS 9
+#include <stdlib.h>
+#include <time.h>
 
-int gerarSudoku(){
-    
-    return 0;
+#define N 9
+
+int tabuleiro[N][N];
+
+// Função pra verificar se um número pode ser colocado na posição
+
+int pode_numero(int row, int col, int num) {
+    // Setor pra verificar a linha
+    for (int x = 0; x < N; x++) {
+        if (tabuleiro[row][x] == num)
+            return 0;
+    }
+
+    // Setor pra verificar a coluna
+    for (int x = 0; x < N; x++) {
+        if (tabuleiro[x][col] == num) {
+            return 0;
+        }
+    }
+
+    // Setor pra verificar o bloco 3x3
+    int startRow = row - row % 3;
+    int startCol = col - col % 3;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (tabuleiro[startRow + i][startCol + j] == num) {
+                return 0;
+            }
+        }
+    }
+
+    return 1;
 }
-
-int main(void) {
-    int sudoku[LINHAS][COLUNAS] = {
-    {0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0}
-    };
-    /* use gerarSudoku() as needed */
-    return 0;
+ // Função pra encontrar o próximo vazio
+int ache_o_vazio(int * row, int * col){
+    for (int i = 0; i < N; i++) {
+        for (int j = 0 ; j < N; j++) {
+            if (tabuleiro[i][j] == 0) {
+                *row = i;
+                *col = j;
+                return 1; 
+            }
+        }
+    }
 }
